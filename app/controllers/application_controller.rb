2a4051e2672d3ad1b3d344258171d6d5c277a426
Onur Uyar <fluxproject@gmail.com>
2011-12-19 14:54:35 +0200

    tracker controller fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
   helper_method :tracker_restart
   
   def tracker_restart
@@ -8,7 +9,7 @@ class ApplicationController < ActionController::Base
       auth = { :username => "", :password => ENV['TWITTER_API_KEY']}
       options = {:query =>{:type => "worker"}, :basic_auth => auth}
       uri = "https://api.heroku.com/apps/proposals-worker/ps/restart"
-      return :json => HTTParty.post(uri,options)
+      return HTTParty.post(uri,options)
     end
     
   end
--- app/controllers/tracker_controller.rb
+++ app/controllers/tracker_controller.rb
@@ -3,18 +3,8 @@ class TrackerController < ApplicationController
   respond_to :json
   
   def restart
-    
-    if Rails.env.production?
-      auth = { :username => "", :password => ENV['TWITTER_API_KEY']}
-      options = {:query =>{:type => "worker"}, :basic_auth => auth}
-      uri = "https://api.heroku.com/apps/proposals-worker/ps/restart"
-      response = HTTParty.post(uri,options)
-    else
-      # Thread.new { system('bundle exec rvmsudo restart proposal-worker-1') }
-      response = {:status => "OK"} 
-    end
-    
-    respond_with response
+
+    respond_with tracker_restart
     
   end
   
