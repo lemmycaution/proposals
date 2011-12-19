ba21880bf6b9a9bcc3ccaf2f00168b1720740d1d
Onur Uyar <fluxproject@gmail.com>
2011-12-19 12:32:15 +0200

    tracker/restart fix

--- app/controllers/tracker_controller.rb
+++ app/controllers/tracker_controller.rb
@@ -2,7 +2,7 @@ class TrackerController < ApplicationController
   def restart
     auth = { :username => "", :password => ENV['TWITTER_API_KEY']}
     options = {:query =>{:type => "worker"}, :basic_auth => auth}
-    uri = "https://api.heroku.com/apps/proposals-tracker/ps/restart"
+    uri = "https://api.heroku.com/apps/proposals-worker/ps/restart"
     render :json => HTTParty.post(uri,options)
   end
 end
