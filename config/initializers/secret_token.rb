721f2dd86d1f9f5944cb8826896e9115c28e6b3c
Onur Uyar <fluxproject@gmail.com>
2011-12-19 06:00:30 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Proposals::Application.config.secret_token = '3efa396c8505b3b38b3e88f4f5b472c1a9a34e6a173ddb7461293e7b7c0f3f52d0fcf271b4f93ec9bf1e6a10f2cf2e2306a4235a01ee9beb5ebeae3b3f9b678a'
